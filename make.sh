8939a2082469f5ed47d59b091c1a1ab4d1848f43
amcampbell1975 <amcampbellgithub@amcampbell.com>
2026-01-11 16:24:31 +0000

    Change dad to game

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ cd ..
 
 echo "Compiling src"
 
-gcc ./src/dad3.c ./x11/draw.c   ./box2d/*.o   -I./box2d/ -lm -lX11 -o dad3
+gcc ./src/game.c ./x11/draw.c   ./box2d/*.o   -I./box2d/ -lm -lX11 -o game
